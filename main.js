5588a76cb98ddfc40c81e08627d4409d8f781b76
Jesus Borjas <jesusbor006@gmail.com>
2022-10-27 14:11:45 -0500

    Test

--- main.js
+++ main.js
@@ -9,22 +9,17 @@ var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana 
 var letsCookBtn = document.querySelector('.btn-1');
 var cookPotImg = document.querySelector('.cookpot-img');
 var radio = document.querySelector('.radio')
-// var sideRadioBtn = document.querySelector('#radio-1');
-// var mainRadioBtn = document.querySelector('#radio-2')
-// var dessertRadioBtn = document.querySelector('#radio-3')
-// var entireRadioBtn = document.querySelector('#radio-4')
-
 
 letsCookBtn.addEventListener('click', hidesPot); 
 
 function hidesPot() {
   cookPotImg.classList.add('hidden');
-  // console.log('clicked');
   let radio = document.forms[0];
 
   for(var i = 0; i < radio.length; i++) {
     if(radio[i].checked) {
       console.log(radio[i].value)
+      
     }
   }
 
