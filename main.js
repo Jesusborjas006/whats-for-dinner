efc0d1c8a6cf1382486abfc1ee4caf8c082e2be9
Jesus Borjas <jesusbor006@gmail.com>
2022-10-26 21:09:04 -0500

    Removes cookPot image

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Data
-
 var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad',
 'Crispy Potatoes', 'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Mushrooms', 'Hush Puppies'];
 
@@ -7,6 +6,16 @@ var mains = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai 
 
 var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread', 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin', 'Croissants', 'Eclairs'];
 
+var letsCookBtn = document.querySelector('.btn-1');
+var cookPotImg = document.querySelector('.cookpot-img');
+
+
+letsCookBtn.addEventListener('click', hidesPot); 
+
+function hidesPot() {
+  cookPotImg.classList.add('hidden');
+  console.log('clicked');
+}
 
 
 
